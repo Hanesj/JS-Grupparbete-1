0bd68edecdaf524bf4e525dc9c15716285470e34
Hanesj <hannes.sjostrom96@gmail.com>
2025-01-08 13:02:59 +0100

    More JS

--- js/components/task2.js
+++ js/components/task2.js
@@ -1,4 +1,8 @@
+//import { subTask } from './subtask2';
+
 const btn = document.querySelector('#submitBtn');
+
+const testCase = document.querySelector('#testCase');
 
 const taskInput = document.querySelector('#task');
 const deadlineInput = document.querySelector('#deadline');
@@ -6,6 +10,13 @@ const statusInput = document.querySelector('#status');
 const prioInput = document.querySelector('#prio');
 const tableBody = document.querySelector('tbody');
 const inputRow = document.querySelector('#inputRow');
+const subTask = document.querySelector('#subTask');
+
+//Subtask input vars
+
+const sStatus = statusInput.cloneNode(true);
+const sDeadlineInput = deadlineInput.cloneNode(true);
+const sPrioInput = prioInput.cloneNode(true);
 
 const addTask = () => {
 	const allInput = {
@@ -14,10 +25,15 @@ const addTask = () => {
 		Status: statusInput.value,
 		Prio: prioInput.value,
 	};
-	task = console.log(allInput);
+	//task = console.log(allInput);
 	createRow(allInput);
 
 	allInput.value = {};
+	taskInput.value = '';
+	//console.log(statusInput);
+	statusInput.selectedIndex = 0;
+	prioInput.selectedIndex = 1;
+	deadlineInput.value = '';
 };
 const createButton = (row) => {
 	const button = document.createElement('button');
@@ -30,12 +46,54 @@ const createButton = (row) => {
 	return button;
 };
 
+const createSubTaskBtn = (row) => {
+	//-------------------- Forsatt har-------------//
+	const button = document.createElement('button');
+	button.textContent = '+';
+	button.className = 'table-button';
+	button.style.background = '#E88437';
+	button.addEventListener('click', () => createSubTask(row));
+	return button;
+};
+// Valja ratt rad
+const createSubTask = (row) => {
+	const newRow = document.createElement('tr');
+	const newCell = document.createElement('td');
+	const input = document.createElement('input');
+
+	const addMoreSubtask = document.createElement('td');
+	addMoreSubtask.appendChild(createSubTaskBtn(row));
+
+	newCell.appendChild(input);
+
+	newRow.appendChild(newCell);
+	newRow.appendChild(addMoreSubtask);
+	newRow.appendChild(sStatus);
+
+	console.log(newRow.cells.length);
+	tableBody.insertBefore(newRow, row.nextSibling);
+
+	//tableBody.insertAdjacentElement(row, newRow);
+	if (newRow.rowIndex % 2 == 0) {
+		newCell.className = 'subba1';
+	} else {
+		newCell.className = 'subba2';
+	}
+	//console.log(newRow.rowIndex);
+};
+
 const createRow = (values) => {
 	const row = document.createElement('tr');
+	//const subTaskbtn = createSubTaskBtn(row);
 	for (const [key, value] of Object.entries(values)) {
+		if (key === 'Deadline') {
+			subBtnCell = document.createElement('td');
+			subBtnCell.appendChild(createSubTaskBtn(row));
+			row.appendChild(subBtnCell);
+		}
 		if (value === '') {
-			alert(`${key} cant be empty.`);
-			return;
+			//alert(`${key} cant be empty.`);
+			//return;
 		} else {
 			const cell = document.createElement('td');
 			cell.appendChild(document.createTextNode(value));
@@ -43,11 +101,16 @@ const createRow = (values) => {
 		}
 	}
 	btnCell = document.createElement('td');
-	btnCell.appendChild(createButton(row));
 
+	btnCell.appendChild(createButton(row));
 	row.appendChild(btnCell);
-	//row.appendChild(c1);
 	tableBody.insertBefore(row, inputRow);
 };
 
+/*const createSubTask = (row, task) => {
+	const subTaskCell = document.createElement('td');
+	row.appendChild(subTaskCell);
+};*/
+
 btn.addEventListener('click', () => addTask(taskInput));
+//testCase.addEventListener('click', () => createSubTask(newRow));
